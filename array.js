6f5437684a3a0efda55c7ffdd6a9efbb1b522427
marqueskennedy-netizen <marques.kennedy@escola.pr.gov.br>
2026-03-17 09:06:55 -0300

    17/03/2026 array

---
+++ array.js
@@ -0,0 +1,15 @@
+let nome = ["Carlos", "Adrian", "Ronaldo", "Raimuno"];
+//             0         1          2          3
+console.log(numeros(1));
+
+let numeros = [1,2,3,4];
+//             0 1 2 3
+
+let matriz=[
+//. 0 1 2 3
+ [20,30,40,50],//0
+
+
+
+
+]
