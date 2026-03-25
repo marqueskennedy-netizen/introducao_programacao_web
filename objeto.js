489dcc8f93eba0ccf9d0b2dd2fa75e27ea345d62
marqueskennedy-netizen <marques.kennedy@escola.pr.gov.br>
2026-03-25 11:04:52 -0300

    25/03/2026

---
+++ objeto.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "João",
+    idade: 30,
+    profissao: "programador"
+
+}
+console.log(pessoa.idade);
+
---
+++ objeto2.js
@@ -0,0 +1,19 @@
+let carro ={
+    marca: "Nissan",
+    modelo: "kicks",
+    ano: 2002,
+
+    ligar: function(){
+        console,log("O carro ligou ");
+    },
+mostrarInfo: function(){
+    console.log('Marca ${this.marca},\nModelo: ${this.modelo}')
+
+
+
+}
+
+
+
+
+}
