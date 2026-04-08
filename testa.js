9aaaf4053d2d6c612799f995f2ebaa68d53a4a94
marqueskennedy-netizen <marques.kennedy@escola.pr.gov.br>
2026-04-08 11:06:04 -0300

    nielamento

---
+++ testa.js
@@ -0,0 +1,23 @@
+//1.exiba a menagem "ola mundo" ao console.
+console.log("Ola Muno");
+//2. Crie uma variavel com seu nome e exiba.
+let nome = "Kennedy";
+console.log(nome);
+//3. some dois numeros e exiba o reultao.
+let soma = 2 + 1;
+console.log(soma)
+//4. Subtraia dois numeros e exiba o resultado.
+let subtraçao = 2-1;
+console.log(subtracao);
+//5. Multiplique dos numeros e exia o resultado.
+let multiplicacao = 2 * 1;
+console.log(multiplicacao)
+//6. Descubra o resto da divisao e de um numero para o outro.
+let resto = 10 % 3;
+console.log(resto);
+//7. Crie uma boleana ( true e false) e exiba seu valor
+let estaChoveo = false;
+console.log(etaChovendo);
+//8. verifique se um numero e maior que o outro
+let verificar = 2, 1;
+onloadeddata.log
